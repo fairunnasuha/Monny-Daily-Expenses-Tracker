7e94c42fd35e6e318c0093e0c2e1e596eefe8e62
fitrahh18 <fitrahbahsri01@gmail.com>
2022-06-21 22:53:59 +0800

    profile

--- app/src/main/java/com/example/monny/homemyexpense.java
+++ app/src/main/java/com/example/monny/homemyexpense.java
@@ -1,10 +1,13 @@
 package com.example.monny;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class homemyexpense extends AppCompatActivity {
@@ -22,7 +25,22 @@ public class homemyexpense extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.setting_menu, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.setting_menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.profilee)
+        {
+            Intent intent = new Intent(homemyexpense.this,Profilepage.class);
+            startActivity(intent);
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
